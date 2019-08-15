91572817a9c4ea166532c4d4851e2e683a58ca2a
javaengineer <1530594258@qq.com>
2019-08-15 10:28:25 +0800

    框架搭建

---
+++ src/components/page/BaseCharts.vue
@@ -0,0 +1,323 @@
+<template>
+    <section class="chart-container">
+        <el-row>
+            <el-col :span="12">
+                <div id="chartColumn" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartBar" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartLine" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="12">
+                <div id="chartPie" style="width:100%; height:400px;"></div>
+            </el-col>
+            <el-col :span="24">
+                <a href="http://echarts.baidu.com/examples.html" target="_blank" style="float: right;">more>></a>
+            </el-col>
+        </el-row>
+    </section>
+</template>
+
+<script>
+    import echarts from 'echarts'
+    export default {
+        data() {
+            return {
+                chartColumn: null,
+                chartBar: null,
+                chartLine: null,
+                chartPie: null
+            }
+        },
+        methods: {
+            drawColumnChart() {
+                this.chartColumn = echarts.init(document.getElementById('chartColumn'));
+                this.chartColumn.setOption({
+                    title: { text: 'Column Chart' },
+                    tooltip: {},
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                });
+            },
+            drawBarChart() {
+                this.chartBar = echarts.init(document.getElementById('chartBar'));
+                this.chartBar.setOption({
+                    title: {
+                        text: 'Bar Chart',
+                        subtext: '数据来自网络'
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'shadow'
+                        }
+                    },
+                    legend: {
+                        data: ['2011年', '2012年']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    },
+                    yAxis: {
+                        type: 'category',
+                        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+                    },
+                    series: [
+                        {
+                            name: '2011年',
+                            type: 'bar',
+                            data: [18203, 23489, 29034, 104970, 131744, 630230]
+                        },
+                        {
+                            name: '2012年',
+                            type: 'bar',
+                            data: [19325, 23438, 31000, 121594, 134141, 681807]
+                        }
+                    ]
+                });
+            },
+            drawLineChart() {
+                this.chartLine = echarts.init(document.getElementById('chartLine'));
+                this.chartLine.setOption({
+                    title: {
+                        text: 'Line Chart'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: ['邮件营销', '联盟广告', '搜索引擎']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [
+                        {
+                            name: '邮件营销',
+                            type: 'line',
+                            stack: '总量',
+                            data: [120, 132, 101, 134, 90, 230, 210]
+                        },
+                        {
+                            name: '联盟广告',
+                            type: 'line',
+                            stack: '总量',
+                            data: [220, 182, 191, 234, 290, 330, 310]
+                        },
+                        {
+                            name: '搜索引擎',
+                            type: 'line',
+                            stack: '总量',
+                            data: [820, 932, 901, 934, 1290, 1330, 1320]
+                        }
+                    ]
+                });
+            },
+            drawPieChart() {
+                this.chartPie = echarts.init(document.getElementById('chartPie'));
+                this.chartPie.setOption({
+                    title: {
+                        text: 'Pie Chart',
+                        subtext: '纯属虚构',
+                        x: 'center'
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        left: 'left',
+                        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+                    },
+                    series: [
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '60%'],
+                            data: [
+                                { value: 335, name: '直接访问' },
+                                { value: 310, name: '邮件营销' },
+                                { value: 234, name: '联盟广告' },
+                                { value: 135, name: '视频广告' },
+                                { value: 1548, name: '搜索引擎' }
+                            ],
+                            itemStyle: {
+                                emphasis: {
+                                    shadowBlur: 10,
+                                    shadowOffsetX: 0,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        }
+                    ]
+                });
+            },
+            drawCharts() {
+                this.drawColumnChart()
+                this.drawBarChart()
+                this.drawLineChart()
+                this.drawPieChart()
+            },
+        },
+        mounted: function () {
+            this.drawCharts()
+        },
+        updated: function () {
+            this.drawCharts()
+        }
+    }
+</script>
+
+<style scoped>
+    .chart-container {
+        width: 100%;
+        float: left;
+    }
+    /*.chart div {
+        height: 400px;
+        float: left;
+    }*/
+    .el-col {
+        padding: 30px 20px;
+    }
+</style>
+
+<!--
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-pie-chart"></i> schart图表</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">
+                vue-schart：vue.js封装sChart.js的图表组件。
+                访问地址：<a href="https://github.com/lin-xin/vue-schart" target="_blank">vue-schart</a>
+            </div>
+            <div class="schart-box">
+                <div class="content-title">柱状图</div>
+                <schart class="schart" canvasId="bar" :data="data1" type="bar" :options="options1"></schart>
+            </div>
+            <div class="schart-box">
+            <div class="content-title">折线图</div>
+            <schart class="schart" canvasId="line" :data="data1" type="line" :options="options2"></schart>
+            </div>
+            <div class="schart-box">
+            <div class="content-title">饼状图</div>
+            <schart class="schart" canvasId="pie" :data="data2" type="pie" :options="options3"></schart>
+            </div>
+            <div class="schart-box">
+            <div class="content-title">环形图</div>
+            <schart class="schart" canvasId="ring" :data="data2" type="ring" :options="options4"></schart>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Schart from 'vue-schart';
+    export default {
+        name: 'basecharts',
+        components: {
+            Schart
+        },
+        data: () => ({
+            data1:[
+                {name:'2012',value:1141},
+                {name:'2013',value:1499},
+                {name:'2014',value:2260},
+                {name:'2015',value:1170},
+                {name:'2016',value:970},
+                {name:'2017',value:1450}
+            ],
+            data2 : [
+                {name:'短袖',value:1200},
+                {name:'休闲裤',value:1222},
+                {name:'连衣裙',value:1283},
+                {name:'外套',value:1314},
+                {name:'羽绒服',value:2314}
+            ],
+            options1: {
+                title: '某商店近年营业总额',
+                autoWidth: true,   // 设置宽高自适应
+                showValue: false,
+                bgColor: '#F9EFCC',
+                fillColor: '#00887C',
+                contentColor: 'rgba(46,199,201,0.3)',
+                yEqual: 7
+            },
+            options2: {
+                title: '某商店近年营业总额',
+                bgColor: '#D5E4EB',
+                titleColor: '#00887C',
+                fillColor: 'red',
+                contentColor: 'rgba(46,199,201,0.3)'
+            },
+            options3: {
+                title: '某商店各商品年度销量',
+                bgColor: '#829dca',
+                titleColor: '#ffffff',
+                legendColor: '#ffffff',
+                radius: 120
+            },
+            options4: {
+                title: '某商店各商品年度销量',
+                bgColor: '#829daa',
+                titleColor: '#ffffff',
+                legendColor: '#ffffff',
+                radius: 120,
+                innerRadius:80
+            }
+        })
+    }
+</script>
+
+<style scoped>
+.schart-box{
+    display: inline-block;
+    margin: 20px;
+}
+    .schart{
+        width: 500px;
+        height: 400px;
+    }
+    .content-title{
+        clear: both;
+        font-weight: 400;
+        line-height: 50px;
+        margin: 10px 0;
+        font-size: 22px;
+        color: #1f2f3d;
+    }
+    
+</style>-->
